a29d7eda3e8099b1aeafa24bf3b06d3c1d595c6b
Raj Negi <razznegi123457@gmail.com>
2024-12-29 19:59:44 +0530

    preparing for railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-jqggbu_8owiqzz=mi8s^v2f+jff&!)1vkcgg%1hwjcy9r2x$o6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app"
+]
+
+if DEBUG:
+    ALLOWED_HOSTS += [
+        'localhost',
+        '127.0.0.1'
+    ]
 
 
 # Application definition
